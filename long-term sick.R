93dee6b52ea662a0d4a672b4cc91c97793805d18
(DHSC) Edward Beake <Edward.Beake_DHSC@phe.gov.uk>
2023-08-02 16:10:31 +0100

    mmake changes to file read in functions for datalake use

--- long-term sick.R
+++ long-term sick.R
@@ -2,13 +2,10 @@ library(tidyverse)
 library(scales)
 library(haven)
 
-hse_2019 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-Healthy Weight, Food & Nutrition Analysis - Data/HSfE/Raw/2019/UKDA-8860-spss/spss/spss25/hse_2019_eul_20211006.sav")
-
-hse_2018 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-Healthy Weight, Food & Nutrition Analysis - Data/HSfE/Raw/2018/UKDA-8649-spss/spss/spss25/hse_2018_eul_22052020.sav")
-
-hse_2017 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-Healthy Weight, Food & Nutrition Analysis - Data/HSfE/Raw/2017/UKDA-8488-spss/spss/spss24/hse17i_eul_v1.sav")
-
-hse_2016 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-Healthy Weight, Food & Nutrition Analysis - Data/HSfE/Raw/2016/UKDA-8334-spss/spss/spss19/hse2016_eul.sav")
+hse_2019 <- read_spss("Z:/IA_HealthSurveyEngland/Data/HSE 2019 SL 20220330.sav")
+hse_2018 <- read_spss("Z:/IA_HealthSurveyEngland/Data/HSE 2018 SL 20220412.sav")
+hse_2017 <- read_dta("Z:/IA_HealthSurveyEngland/Data/hse2017_eul.dta")
+hse_2016 <- read_spss("Z:/IA_HealthSurveyEngland/Data/hse2016_eul.sav")
 
 # economic activity status by BMI
  hse_2018 %>% 
@@ -21,11 +18,11 @@ hse_2016 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-H
                                      HRPactIv2 == 2 ~ 'healthy',
                                      HRPactIv2 == 3 ~ 'healthy',
                                      HRPactIv2 == 4 ~ 'healthy',
-                                     HRPactIv2 == 5 ~ 'non-healthy',
-                                     HRPactIv2 == 6 ~ 'non-healthy',
+                                     HRPactIv2 == 5 ~ 'healthy',
+                                     HRPactIv2 == 6 ~ 'healthy',
                                      HRPactIv2 == 7 ~ 'long-term sick',
-                                     HRPactIv2 == 8 ~ 'non-healthy',
-                                     HRPactIv2 == 9 ~ 'non-healthy')) %>% 
+                                     HRPactIv2 == 8 ~ 'healthy',
+                                     HRPactIv2 == 9 ~ 'healthy')) %>% 
   filter(!is.na(Economic_Status)) %>% # remove other activity
   group_by(BMI_cat,Economic_Status) %>% 
   summarise(num = sum(wt_int)) %>% 
@@ -71,11 +68,11 @@ hse_2016 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-H
                                        HRPactiv2 == 2 ~ 'healthy',
                                        HRPactiv2 == 3 ~ 'healthy',
                                        HRPactiv2 == 4 ~ 'healthy',
-                                       HRPactiv2 == 5 ~ 'non-healthy',
-                                       HRPactiv2 == 6 ~ 'non-healthy',
+                                       HRPactiv2 == 5 ~ 'healthy',
+                                       HRPactiv2 == 6 ~ 'healthy',
                                        HRPactiv2 == 7 ~ 'long-term sick',
-                                       HRPactiv2 == 8 ~ 'non-healthy',
-                                       HRPactiv2 == 9 ~ 'non-healthy')) %>% 
+                                       HRPactiv2 == 8 ~ 'healthy',
+                                       HRPactiv2 == 9 ~ 'healthy')) %>% 
     filter(!is.na(Economic_Status)) %>% # remove other activity
     group_by(BMI_cat,Economic_Status) %>% 
     summarise(num = sum(wt_int)) %>% 
@@ -96,11 +93,11 @@ hse_2016 <- read_sav("C:/Users/EBeake/Department of Health and Social Care/GOV-H
           Economic_Status = case_when(HRPactiv3 == 1 ~ 'healthy', # activity status
                                       HRPactiv3 == 2 ~ 'healthy',
                                       HRPactiv3 == 3 ~ 'healthy',
-                                      HRPactiv3 == 4 ~ 'non-healthy',
-                                      HRPactiv3 == 5 ~ 'non-healthy',
-                                      HRPactiv3 == 6 ~ 'non-healthy',
-                                      HRPactiv3 == 7 ~ 'long-term sick',
-                                      HRPactiv3 == 8 ~ 'non-healthy')) %>% 
+                                      HRPactiv3 == 4 ~ 'healthy',
+                                      HRPactiv3 == 5 ~ 'healthy',
+                                      HRPactiv3 == 6 ~ 'long-term sick',
+                                      HRPactiv3 == 7 ~ 'healthy',
+                                      HRPactiv3 == 8 ~ 'healthy')) %>% 
    filter(!is.na(Economic_Status)) %>% # remove other activity
    group_by(BMI_cat,Economic_Status) %>% 
    summarise(num = sum(wt_int)) %>% 
